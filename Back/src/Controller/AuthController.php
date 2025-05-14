9bbe71eba4ac7eb160fc5070bc1588408914e145
Faxiis <antho.lavoute@gmail.com>
2025-05-14 10:23:33 +0200

    Ajout de l'authentification, register et login

---
+++ Back/src/Controller/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AuthController extends AbstractController
+{
+    public function __construct(
+        private EntityManagerInterface $em,
+        private UserRepository $userRepository,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {}
+
+    #[Route('/register', name: 'app_register', methods: ['POST'])]
+    public function register(Request $request): JsonResponse
+    {
+        // Récupération de la requête
+        $data = json_decode($request->getContent(), true);
+        $username = $data['username'] ?? null;
+        $password = $data['password'] ?? null;
+
+        // Vérifiez si l'utilisateur existe déjà
+        if ($this->userRepository->findOneBy(['username' => $username])) {
+            return $this->json(['error' => 'User already exists'], 400);
+        }
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword(
+            $this->passwordHasher->hashPassword($user, $password)
+        );
+        $user->setRoles(['ROLE_USER']);
+
+        // Enregistrez l'utilisateur
+        $this->em->persist($user);
+        $this->em->flush();
+
+        return $this->json(['message' => 'User registered successfully']);
+    }
+
+    #[Route('/login', name: 'app_login', methods: ['POST'])]
+    public function login(Request $request): JsonResponse
+    {
+        // Récupération de la requête
+        $data = json_decode($request->getContent(), true);
+        $username = $data['username'] ?? null;
+        $password = $data['password'] ?? null;
+
+        // Vérifiez si l'utilisateur existe déjà
+        if (!$this->userRepository->findOneBy(['username' => $username])) {
+            return $this->json(['error' => 'Wrong username or password'], 400);
+        }
+
+        // Vérifiez le mot de passe
+        $user = $this->userRepository->findOneBy(['username' => $username]);
+        if (!$this->passwordHasher->isPasswordValid($user, $password, null, null)) {
+            return $this->json(['error' => 'Wrong username or password'], 400);
+        }
+
+        return $this->json(['message' => 'User logged successfully']);
+    }
+}

333e18f9c08ebd2b4d8cd75afbef8027a5778795
Faxiis <antho.lavoute@gmail.com>
2025-05-14 09:25:39 +0200

    Ajout fixtures

---
+++ Back/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Tweet;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $passwordEncoder;
+
+    public function __construct(UserPasswordHasherInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker::create();
+
+        $users = [];
+
+        // Créer une dizaine d'utilisateurs
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setUsername('user' . $i);
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword($this->passwordEncoder->hashPassword($user, 'password' . $i));
+
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // Créer une centaine de tweets
+        for ($i = 0; $i < 100; $i++) {
+            $tweet = new Tweet();
+            $tweet->setContent(implode(' ', $faker->words(10)));
+            $tweet->setUsr($users[array_rand($users)]); // Utilisateur aléatoire
+
+            // Ajout de likes aléatoires (5 à 10 utilisateurs différents)
+            $likeIndices = array_rand($users, rand(5, 10));
+            if (!is_array($likeIndices)) {
+                $likeIndices = [$likeIndices];
+            }
+            foreach ($likeIndices as $index) {
+                $tweet->addLike($users[$index]);
+            }
+
+            $manager->persist($tweet);
+        }
+
+        $manager->flush();
+    }
+
+}

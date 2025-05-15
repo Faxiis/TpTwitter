1c0550bdeeb0da4fe90a9ad80fc46f943bce63d8
Dimoscore <mathgascon@laposte.net>
2025-05-15 14:04:01 +0200

    Version Jour 3

--- front/src/Controller/AccueilController.php
+++ front/src/Controller/AccueilController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
-class LogController extends AbstractController
+class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
     public function index(EntityManagerInterface $entityManager): Response
